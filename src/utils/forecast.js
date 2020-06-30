b7cfc2fede8a96b588392e61799c77ebfc11f809
thaoducphung <thaoducphung@gmail.com>
2020-06-30 18:47:42 +0700

    Add % in humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,7 +16,7 @@ const forecast = (latitude,longitude,callback) =>{
                 '. It is currently '+body.current.temperature+
                 ' degrees (Feelslike: '+body.current.feelslike+
                 '). There is a '+body.current.precip+'% chance of rain. The hudmidity is '
-                +body.current.humidity+', and the UV index is '+body.current.uv_index+'.'
+                +body.current.humidity+'%, and the UV index is '+body.current.uv_index+'.'
             // {
             //     weather_descriptions: body.current.weather_descriptions[0],
             //     temperature: body.current.temperature,
